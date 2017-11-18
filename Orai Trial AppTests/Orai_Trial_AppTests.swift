aeac58944ea6f7012a05ee9d262ad42cde273e5d
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-18 09:57:14 -0500

    Initial Commit

---
+++ Orai Trial AppTests/Orai_Trial_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Orai_Trial_AppTests.swift
+//  Orai Trial AppTests
+//
+//  Created by Chris Thompson on 11/18/17.
+//  Copyright © 2017 Chris Thompson. All rights reserved.
+//
+
+import XCTest
+@testable import Orai_Trial_App
+
+class Orai_Trial_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
