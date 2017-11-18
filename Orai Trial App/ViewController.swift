847a06b3c2a684b391985de26968a96c6ff104fa
chris-thompson13 <chris@hiassistant.comgit config --global user.email chris@hiassistant.com>
2017-11-18 10:19:40 -0500

    first commit

--- Orai Trial App/ViewController.swift
+++ Orai Trial App/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var record: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func recordAction(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
